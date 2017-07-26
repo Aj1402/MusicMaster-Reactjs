728129a5bf8265c7a0af4eae2178f9cca70e2d3e
Arjun Joshi <itscoolarjun@hotmail.com>
2017-07-26 13:35:39 +0530

    Fixed bugs, removed yarn, Initialized App Component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Music master from App</div>
+    );
+  }
+}
+
+export default App;
